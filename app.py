c75d508d670c53ab2553f1263ec17a33f359aded
mdnasirkhanjd-netizen <mdnasirkhanjd@gmail.com>
2026-08-23 22:38:35 +0600

    app.py

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ from gradio_client import Client, handle_file
 
 app = FastAPI()
 
-# HuggingFace Client
 hf_client = Client("mrfakename/E2-F5-TTS")
 
 REF_AUDIO = "my_voice.mp3"
@@ -21,15 +20,14 @@ async def generate_audio(request: Request):
     data = await request.json()
     script_text = data.get("text", "")
     
-    # api_name এবং কি-ওয়ার্ড আর্গুমেন্ট ছাড়া সরাসরি পজিশনাল কল
     result = hf_client.predict(
-        handle_file(REF_AUDIO),  # 1. ref_audio
-        REF_TEXT,                # 2. ref_text
-        script_text,             # 3. gen_text
-        True,                    # 4. remove_silence
-        0.15,                    # 5. cross_fade_duration
-        32,                      # 6. nfe_step
-        1.0                      # 7. speed
+        handle_file(REF_AUDIO),
+        REF_TEXT,
+        script_text,
+        True,
+        0.15,
+        32,
+        1.0
     )
     return FileResponse(result[0], media_type="audio/wav", filename="output.wav")
 
